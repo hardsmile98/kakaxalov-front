1df1a59b57bea9377d5342f5ae1bfd8bc4ee8af1
Денис Котелев <deniskotelev@MacBook-Pro-Denis.local>
2024-04-28 23:07:08 +0300

    f

--- src/pages/Boosts/index.tsx
+++ src/pages/Boosts/index.tsx
@@ -1,6 +1,44 @@
+import boostsImage from 'assets/images/boostsImage.webp'
+import boostsIcon from 'assets/images/boostsIcon.svg'
+import styles from './styles.module.css'
+
 function Boosts () {
   return (
-    <div>Boosts</div>
+    <div className={styles.root}>
+      <div className={styles.imageWrapper}>
+        <img
+          className={styles.image}
+          src={boostsImage}
+          alt="boosts image"
+        />
+      </div>
+
+      <div className={styles.head}>
+        <img
+          src={boostsIcon}
+          alt="boosts icon"
+        />
+
+        <h2>
+          Boosts
+        </h2>
+        <p>
+          Прокачивай уровни буста и собирай больше КАКАХ
+        </p>
+      </div>
+
+      <div className={styles.blockWrapper}>
+        <h5>
+          Eжеденевно
+        </h5>
+      </div>
+
+      <div className={styles.blockWrapper}>
+        <h5>
+          Бусты
+        </h5>
+      </div>
+    </div>
   )
 }
 
