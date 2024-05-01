7f433c31cae83b839c610dc1d421c32e434a4344
Денис Котелев <deniskotelev@MacBook-Pro-Denis.local>
2024-05-01 23:53:58 +0300

    feat

--- src/pages/Game/Gameplay/index.tsx
+++ src/pages/Game/Gameplay/index.tsx
@@ -1,6 +1,8 @@
 import coinIcon from 'assets/images/improvement2.webp'
 import kakaxaInit from 'assets/images/kakaxaInit.gif'
 import islandBig from 'assets/images/island-big.svg'
+import islandOriginal from 'assets/images/island-original.svg'
+import islandBlack from 'assets/images/island-black.svg'
 import styles from './styles.module.css'
 
 function Gameplay () {
@@ -15,6 +17,12 @@ function Gameplay () {
         </div>
 
         <div className={styles.wrapper}>
+            <img className={styles.islandLeftTop} src={islandBlack} alt="island" />
+            <img className={styles.islandLeftBottom} src={islandOriginal} alt="island" />
+
+            <img className={styles.islandRightTop} src={islandBlack} alt="island" />
+            <img className={styles.islandRightBottom} src={islandOriginal} alt="island" />
+
             <img className={styles.kakaxa} src={kakaxaInit} alt="kakaxa" />
             <img className={styles.islandBig} src={islandBig} alt="island" />
         </div>
