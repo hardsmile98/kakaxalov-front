274bce4d731f5e93bcabf4edc8ea145bcf9908f6
Денис Котелев <deniskotelev@MacBook-Pro-Denis.local>
2024-05-04 00:49:26 +0300

    feat

--- src/pages/Game/Gameplay/index.tsx
+++ src/pages/Game/Gameplay/index.tsx
@@ -111,7 +111,7 @@ function Gameplay () {
     if (timeoutRef.current === null && coinPosition === null) {
       timeoutRef.current = setTimeout(
         () => generateCoin(),
-        randomInteger(2_000, 5_000)
+        randomInteger(2_000, 4_000)
       )
     }
   }, [generateCoin, coinPosition])
