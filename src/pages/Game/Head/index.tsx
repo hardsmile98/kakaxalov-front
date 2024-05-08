d30068186e5a8827c6187361edb43c36540d30a4
Денис Котелев <d.kotelev@carprice.ru>
2024-05-08 13:27:25 +0300

    feat

--- src/pages/Game/Head/index.tsx
+++ src/pages/Game/Head/index.tsx
@@ -1,16 +1,20 @@
 import coinIcon from 'assets/images/improvement2.webp'
 import helth from 'assets/images/helth.svg'
+import { useAppSelector } from 'store'
+import { gameSettings } from 'constants/index'
 import styles from './styles.module.css'
 
 function Head () {
-  const countHelth = 3
+  const helths = useAppSelector(state => state.game.helths)
+  const score = useAppSelector(state => state.game.score)
+  const coin = useAppSelector(state => state.game.coin)
 
   return (
     <div className={styles.head}>
       <div className={styles.wrapper}>
         <div className={styles.helthsWrapper}>
           <div className={styles.helths}>
-            {new Array(countHelth).fill(null)
+            {new Array(helths).fill(null)
               .map((_, idx) => <img
                 key={idx}
                 src={helth}
@@ -22,14 +26,20 @@ function Head () {
         <div className={styles.coinWrapper}>
           <img className={styles.coin} src={coinIcon} alt='coin' />
           <span>
-            21.324 КАКАХ
+            {score} КАКАХ
           </span>
         </div>
       </div>
 
       <div className={styles.progressbar}>
-        <span className={styles.line} style={{ width: '30%' }} />
-        <span className={styles.score}>53 / 100</span>
+        <span
+          className={styles.line}
+          style={{ width: `${(coin / gameSettings.LIMIT_COLLECTION) * 100}%` }}
+        />
+
+        <span className={styles.score}>
+          {coin} / {gameSettings.LIMIT_COLLECTION}
+        </span>
       </div>
     </div>
   )
