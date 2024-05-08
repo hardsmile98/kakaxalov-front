4d7a3a0143e9f5653c17194b3d8eeb5c7667e1ed
Денис Котелев <d.kotelev@carprice.ru>
2024-05-08 13:14:32 +0300

    feat

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,3 +1,4 @@
+import { type TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import gameReducer from './slices/game'
 import { configureStore } from '@reduxjs/toolkit'
 
@@ -9,3 +10,14 @@ const store = configureStore({
 })
 
 export default store
+
+type RootState = ReturnType<typeof store.getState>
+type AppDispatch = typeof store.dispatch
+
+const useAppDispatch: () => AppDispatch = useDispatch
+const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+
+export {
+  useAppDispatch,
+  useAppSelector
+}
