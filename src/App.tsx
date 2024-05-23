ebfc59aa60c71e62b194fdc23c69a6e2d83ae637
Денис Котелев <d.kotelev@carprice.ru>
2024-05-23 11:40:38 +0300

    f

--- src/App.tsx
+++ src/App.tsx
@@ -31,6 +31,8 @@ function App () {
 
     const tgInitData = 'query_id=AAHkvS4sAgAAAOS9LixFYU1F&user=%7B%22id%22%3A5036228068%2C%22first_name%22%3A%22Denis%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22deniskotelev%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1715626450&hash=4bab21f94b38bbfa79692df9fac67f84daf63a66bb66badbf3063675d0f2c4dd'
     window.localStorage.setItem('tgData', tgInitData)
+
+    alert(tgInitData)
   }, [tg])
 
   useEffect(() => {
