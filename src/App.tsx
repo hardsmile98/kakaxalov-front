390a4e3e8ef39e1ecb71dabba7079d0def7ef1a4
Денис Котелев <deniskotelev@MacBook-Pro-Denis.local>
2024-05-26 23:32:28 +0300

    f

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ function App () {
   useEffect(() => {
     const tgInitData = isDev() ? envs.testInitData : tg.initData
     window.localStorage.setItem('tgData', tgInitData)
-    alert(JSON.stringify(tg.initDataUnsafe))
+    alert(window.location.href)
     setTgLoading(false)
     setTgReady(true)
   }, [tg])
