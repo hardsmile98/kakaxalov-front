59708c0096262a3211b6cabe5187b4a775c2be9f
Денис Котелев <d.kotelev@carprice.ru>
2024-07-05 10:47:01 +0300

    f

--- src/pages/Game/index.tsx
+++ src/pages/Game/index.tsx
@@ -1,16 +1,20 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import Gameplay from './Gameplay';
 import Head from './Head';
 import Menu from './Menu';
 import styles from './styles.module.css';
 
 function Game() {
+  const ref = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
-    setTimeout(() => window.scrollTo(0, 0), 1_000);
+    if (ref.current) {
+      ref.current.scrollTo(0, 0);
+    }
   }, []);
 
   return (
-    <div className={styles.root}>
+    <div className={styles.root} ref={ref}>
       <div className={styles.wrapper}>
         <Head />
 
