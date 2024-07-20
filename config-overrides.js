cb930a25fcc14b49576d908700ab0278f81f669d
Денис Котелев <deniskotelev@MacBook-Pro-Denis.local>
2024-07-20 21:59:40 +0300

    f

--- config-overrides.js
+++ config-overrides.js
@@ -2,33 +2,34 @@ const WebpackObfuscator = require("webpack-obfuscator");
 
 const getProuctionConfig = (config) => ({
   ...config,
-  // optimization: {
-  //   ...config.optimization,
-  //   splitChunks: {
-  //     cacheGroups: {
-  //       vendor: {
-  //         test: /[\\/]node_modules[\\/]/,
-  //         name: "vendors",
-  //         chunks: "all",
-  //       },
-  //     },
-  //   },
-  // },
-  plugins: [
-    ...config.plugins,
-    new WebpackObfuscator(
-      {
-        rotateStringArray: true,
-        reservedNames: [],
-        reservedStrings: [],
-        splitStrings: true,
-        transformObjectKeys: true,
-        stringArrayCallsTransform: true,
-        stringArrayIndexShift: true,
-        stringArrayWrappersChainedCalls: true,
+  optimization: {
+    ...config.optimization,
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+        },
       },
-    ),
-  ],
+    },
+  },
+  // plugins: [
+  //   ...config.plugins,
+  //   new WebpackObfuscator(
+  //     {
+  //       rotateStringArray: true,
+  //       reservedNames: [],
+  //       reservedStrings: [],
+  //       splitStrings: true,
+  //       transformObjectKeys: true,
+  //       stringArrayCallsTransform: true,
+  //       stringArrayIndexShift: true,
+  //       stringArrayWrappersChainedCalls: true,
+  //     },
+  //     ["static/js/vendors.*.js"]
+  //   ),
+  // ],
 });
 
 module.exports = function override(config, env) {
