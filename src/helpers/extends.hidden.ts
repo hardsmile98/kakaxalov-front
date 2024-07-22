7c7e7494eb2b47ca335b870a3f62bb881435f7d4
Денис Котелев <deniskotelev@MacBook-Pro-Denis.local>
2024-07-22 16:13:39 +0300

    f

--- src/helpers/extends.hidden.ts
+++ src/helpers/extends.hidden.ts
@@ -1,25 +1,31 @@
 import CryptoJS from 'crypto-js';
 import { envs } from 'constants/index';
+import { BaseQueryApi } from '@reduxjs/toolkit/query';
 
 function generateHMACSignature(message: string, secret: string) {
   return CryptoJS.HmacSHA256(message, secret).toString(CryptoJS.enc.Hex);
 }
 
-function prepareHeaders(headers: Headers) {
+type PrepareHeadersParams = {
+  prepareHeadersArgs: { headers: Headers, api: Pick<BaseQueryApi, 'getState' | 'extra' | 'endpoint' | 'type' | 'forced'> }
+  url: string
+};
+
+function prepareHeaders({ prepareHeadersArgs, url }: PrepareHeadersParams) {
   const timestamp = Date.now().toString();
 
   const tgData = localStorage.getItem('tgData');
 
   if (tgData !== null) {
-    const message = `${tgData}:${timestamp}`;
+    const message = `${tgData}:${url}:${timestamp}`;
     const signature = generateHMACSignature(message, envs.testKey);
 
-    headers.set('X-Telegram-Data', tgData);
-    headers.set('X-Timestamp', timestamp);
-    headers.set('X-Signature', signature);
+    prepareHeadersArgs.headers.set('X-Telegram-Data', tgData);
+    prepareHeadersArgs.headers.set('X-Timestamp', timestamp);
+    prepareHeadersArgs.headers.set('X-Signature', signature);
   }
 
-  return headers;
+  return prepareHeadersArgs.headers;
 }
 
 export {
--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,22 +1,51 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import {
+  BaseQueryApi,
+  BaseQueryFn,
+  createApi,
+  fetchBaseQuery,
+} from '@reduxjs/toolkit/query/react';
 import { envs } from 'constants/index';
 import { prepareHeaders } from 'helpers/index';
 import tagTypes from './tagTypes';
 import * as endpoints from './endpoints';
 
+const baseQuery: BaseQueryFn = (...baseQueryArgs) => {
+  const [args] = baseQueryArgs;
+
+  const url: string = args.url || args;
+
+  return fetchBaseQuery({
+    baseUrl: envs.apiUrl,
+    prepareHeaders: (
+      headers: Headers,
+      api: Pick<
+      BaseQueryApi,
+      'getState' | 'extra' | 'endpoint' | 'type' | 'forced'
+      >,
+    ) => prepareHeaders({
+      prepareHeadersArgs: {
+        api,
+        headers,
+      },
+      url,
+    }),
+    credentials: 'include',
+  })(...baseQueryArgs);
+};
+
 export const publicApi = createApi({
   reducerPath: 'publicApi',
-  baseQuery: fetchBaseQuery({
-    baseUrl: envs.apiUrl,
-    prepareHeaders,
-    credentials: 'include',
-  }),
+  baseQuery,
 
   endpoints: (builder) => ({
-    getProfile: builder.query<endpoints.ProfileResponse, undefined>(endpoints.getProfile),
+    getProfile: builder.query<endpoints.ProfileResponse, undefined>(
+      endpoints.getProfile,
+    ),
     getLeadboard: builder.query(endpoints.getLeadboard),
     getTasks: builder.query<endpoints.Tasks, undefined>(endpoints.getTasks),
-    getStats: builder.query<endpoints.StatsResponse, undefined>(endpoints.getStats),
+    getStats: builder.query<endpoints.StatsResponse, undefined>(
+      endpoints.getStats,
+    ),
     completeTask: builder.mutation(endpoints.completeTask),
     getReferals: builder.query(endpoints.getReferals),
     getBoosts: builder.query(endpoints.getBoosts),
@@ -24,9 +53,13 @@ export const publicApi = createApi({
     applyBoost: builder.mutation(endpoints.applyBoost),
     startGame: builder.mutation(endpoints.startGame),
     endGame: builder.mutation(endpoints.endGame),
-    checkEnergy: builder.query<endpoints.CheckEnergyResponse, undefined>(endpoints.checkEnergy),
-    getNftBonus: builder
-      .query<endpoints.GetNftBonusResponse, string | undefined>(endpoints.getNftBonus),
+    checkEnergy: builder.query<endpoints.CheckEnergyResponse, undefined>(
+      endpoints.checkEnergy,
+    ),
+    getNftBonus: builder.query<
+    endpoints.GetNftBonusResponse,
+    string | undefined
+    >(endpoints.getNftBonus),
   }),
 
   tagTypes: Object.values(tagTypes),
